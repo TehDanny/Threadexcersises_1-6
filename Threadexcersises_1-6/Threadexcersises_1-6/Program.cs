a7ffa9e7ba9ff676b975ed938870741a45c4cd72
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-07-04 01:47:10 +0200

    Opgave 1

--- Threadexcersises_1-6/Threadexcersises_1-6/Program.cs
+++ Threadexcersises_1-6/Threadexcersises_1-6/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Threadexcersises_1_6
 {
@@ -10,6 +11,19 @@ namespace Threadexcersises_1_6
     {
         static void Main(string[] args)
         {
+            ThreadStart printMethod1 = new ThreadStart(Print1);
+            Thread thread1 = new Thread(Print1);
+            thread1.Start();
+            Console.ReadKey();
+        }
+
+        private static void Print1()
+        {
+            Console.WriteLine("C#-trådning er nemt!");
+            Console.WriteLine("C#-trådning er nemt!");
+            Console.WriteLine("C#-trådning er nemt!");
+            Console.WriteLine("C#-trådning er nemt!");
+            Console.WriteLine("C#-trådning er nemt!");
         }
     }
 }
