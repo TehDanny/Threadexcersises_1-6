4f0dda8de293faddd9bbccd65d40c7743d516368
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-07-04 01:54:00 +0200

    Opgave 2

--- Threadexcersises_1-6/Threadexcersises_1-6/Program.cs
+++ Threadexcersises_1-6/Threadexcersises_1-6/Program.cs
@@ -12,18 +12,30 @@ namespace Threadexcersises_1_6
         static void Main(string[] args)
         {
             ThreadStart printMethod1 = new ThreadStart(Print1);
-            Thread thread1 = new Thread(Print1);
+            ThreadStart printMethod2 = new ThreadStart(Print2);
+            Thread thread1 = new Thread(printMethod1);
+            Thread thread2 = new Thread(printMethod2);
             thread1.Start();
+            thread2.Start();
             Console.ReadKey();
         }
 
         private static void Print1()
         {
-            Console.WriteLine("C#-trådning er nemt!");
-            Console.WriteLine("C#-trådning er nemt!");
-            Console.WriteLine("C#-trådning er nemt!");
-            Console.WriteLine("C#-trådning er nemt!");
-            Console.WriteLine("C#-trådning er nemt!");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("C#-trådning er nemt!");
+                Thread.Sleep(1000);
+            }
+        }
+
+        private static void Print2()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Også med flere tråde …");
+                Thread.Sleep(1000);
+            }
         }
     }
 }
